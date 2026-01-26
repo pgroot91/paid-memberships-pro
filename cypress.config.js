30e563bfcd865356f00fc578658f1f2773cf9d50
Patrick Groot <info@patrickgroot.com>
2026-01-26 23:52:16 +0100

    added spec files for admin pages, wip

--- cypress.config.js
+++ cypress.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
     openMode: 0,
   },
   e2e: {
-    specPattern: "tests/e2e/cypress/integration/**/*.spec.{js,jsx,ts,tsx}",
+    specPattern: ["tests/e2e/cypress/integration/**/*.spec.{js,jsx,ts,tsx}", "tests/e2e/cypress/api/**/*.spec.{js,jsx,ts,tsx}"],
     supportFile: "tests/e2e/cypress/support/e2e.js",
     setupNodeEvents(on, config) {
       on("before:browser:launch", (browser = {}, launchOptions) => {
