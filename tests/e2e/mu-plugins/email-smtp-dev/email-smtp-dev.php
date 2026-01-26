ce37b85e5edf47ee3747191c7012c6490fea56a9
Patrick Groot <info@patrickgroot.com>
2026-01-26 03:16:46 +0100

    Fix mail

--- tests/e2e/mu-plugins/email-smtp-dev/email-smtp-dev.php
+++ tests/e2e/mu-plugins/email-smtp-dev/email-smtp-dev.php
@@ -10,7 +10,7 @@ function DevConfigMailpit($phpmailer) {
     error_log('📬 PHPMailer init hook triggered');
 
     $phpmailer->isSMTP();
-    $phpmailer->Host       = 'pm-pro-mailpit'; // Mailpit SMTP host
+    $phpmailer->Host       = 'paid-memberships-pro-mailpit'; // Mailpit SMTP host
     $phpmailer->Port       = 1025;                        // Mailpit SMTP port
     $phpmailer->SMTPAuth   = false;                       // No auth for Mailpit by default
     $phpmailer->SMTPSecure = false;                       // No encryption for local SMTP
