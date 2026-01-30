9c4dc77416f99adf47e1c74d289e9b2745cfb7b8
Patrick Groot <info@patrickgroot.com>
2026-01-30 21:14:02 +0100

    Move beforeEach

--- tests/e2e/cypress/integration/admin/addons.spec.ts
+++ tests/e2e/cypress/integration/admin/addons.spec.ts
@@ -1,13 +1,12 @@
 import { ROUTES } from "../../support/routes";
 
 describe("Paid Memberships Pro > Add Ons", { testIsolation: false }, () => {
-  beforeEach("Login and Visit Addons Page", () => {
-    const admin = Cypress.env("admin");
-    cy.loginByForm(admin.username, admin.password);
-    cy.visit(ROUTES.ADMIN_ADDONS);
-  });
-
   context("Search Functionality", { testIsolation: false }, () => {
+    beforeEach("Login and Visit Addons Page", () => {
+      const admin = Cypress.env("admin");
+      cy.loginByForm(admin.username, admin.password);
+      cy.visit(ROUTES.ADMIN_ADDONS);
+    });
     const addons = [
       "Add PayPal Express",
       "Member Network Sites",
@@ -21,6 +20,11 @@ describe("Paid Memberships Pro > Add Ons", { testIsolation: false }, () => {
   });
 
   context("Filter Functionality", { testIsolation: false }, () => {
+    beforeEach("Login and Visit Addons Page", () => {
+      const admin = Cypress.env("admin");
+      cy.loginByForm(admin.username, admin.password);
+      cy.visit(ROUTES.ADMIN_ADDONS);
+    });
     const categories = ["All", "Popular", "Free", "Premium"];
     categories.forEach((category) => {
       it(`Should be able to filter on Add On category: "${category}"`, () => {
@@ -30,11 +34,24 @@ describe("Paid Memberships Pro > Add Ons", { testIsolation: false }, () => {
   });
 
   context("Installation Functionality", { testIsolation: false }, () => {
+    beforeEach("Login and Visit Addons Page", () => {
+      const admin = Cypress.env("admin");
+      cy.loginByForm(admin.username, admin.password);
+      cy.visit(ROUTES.ADMIN_ADDONS);
+    });
     it("Should be able to install an Add On", () => {});
   });
 
   context("No License, Upsell Notice", { testIsolation: false }, () => {
-    const addons = [{name: "Add PayPal Express", licenseType: "Standard"}, {name: "Member Network Sites", licenseType: "Plus"}];
+    beforeEach("Login and Visit Addons Page", () => {
+      const admin = Cypress.env("admin");
+      cy.loginByForm(admin.username, admin.password);
+      cy.visit(ROUTES.ADMIN_ADDONS);
+    });
+    const addons = [
+      { name: "Add PayPal Express", licenseType: "Standard" },
+      { name: "Member Network Sites", licenseType: "Plus" },
+    ];
     addons.forEach((addon) => {
       it(`Should not be able to install "${addon.name} (License: ${addon.licenseType})" without an active license, upsell popup should appear`, () => {
         cy.contains(addon.name);
